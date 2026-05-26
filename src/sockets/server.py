555f4abb1f8cc00a595a268aa236e1a7f82a536a
JulianaEmanuel <jemanuelp@uteq.edu.ec>
2026-05-25 21:03:22 -0500

    feat: agregar soporte multi-puerto y clientes nodo2 y nodo3

--- src/sockets/server.py
+++ src/sockets/server.py
@@ -16,18 +16,42 @@ def handle_client(conn, addr):
             msg = json.loads(data.decode())
             ts = update_clock(msg.get("timestamp", 0))
             print(f"[SERVER] De {msg['sender']} | LT={ts} | msg='{msg['message']}'")
-            response = json.dumps({"sender": "server", "timestamp": ts, "message": "ACK"})
+            response = json.dumps({
+                "sender": "server",
+                "timestamp": ts,
+                "message": "ACK"
+            })
             conn.sendall(response.encode())
 
-def start_server(host="127.0.0.1", port=5000):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((host, port))
-        s.listen(200)
-        print(f"[SERVER] Escuchando en {host}:{port}")
-        while True:
-            conn, addr = s.accept()
-            threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
+def listen_port(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(200)
+
+    print(f"[SERVER] Escuchando en {host}:{port}")
+
+    while True:
+        conn, addr = s.accept()
+        threading.Thread(
+            target=handle_client,
+            args=(conn, addr),
+            daemon=True
+        ).start()
+
+def start_server(host="127.0.0.1"):
+    ports = [5000, 5001, 5002]
+
+    for port in ports:
+        threading.Thread(
+            target=listen_port,
+            args=(host, port),
+            daemon=True
+        ).start()
+
+    while True:
+        pass
 
 if __name__ == "__main__":
     start_server()
+    
